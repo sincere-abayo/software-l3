881ee6a26e69ec162d09ba7fc3fd23024bb6c850
sincere-abayo <abayosincere11@gmail.com>
2024-04-25 13:06:47 +0200

    foreign key

--- auth_app/index.php
+++ auth_app/index.php
@@ -4,15 +4,15 @@ session_start();
 if (!isset($_SESSION['user_email'])) {
    header("location:login.php");
 }
-$user_email= $_SESSION['user_email'];
+$user_email = $_SESSION['user_email'];
 
 include 'config.php';
 
-$selectQuery="SELECT * from users where user_email='$user_email'";
-$executeQuery=mysqli_query($connect,$selectQuery);
-$data=mysqli_fetch_array($executeQuery);
+$selectQuery = "SELECT * from users where user_email='$user_email'";
+$executeQuery = mysqli_query($connect, $selectQuery);
+$data = mysqli_fetch_array($executeQuery);
 
-$user_id=$data['user_id'];
+$user_id = $data['user_id'];
 // echo $user_id;
 
 ?>
@@ -31,7 +31,7 @@ $user_id=$data['user_id'];
    <center>
       <form method="post">
          <h2>
-            create product 
+            create product
 
          </h2>
          <a href="logout.php">logout</a>
@@ -52,35 +52,32 @@ $user_id=$data['user_id'];
       </form>
 
       <?php
-if (isset($_POST['upload'])) {
-  $product=$_POST['product'];
-  $qty=$_POST['qty'];
+      if (isset($_POST['upload'])) {
+         $product = $_POST['product'];
+         $qty = $_POST['qty'];
 
-  $insert="INSERT INTO products values ('','$user_id','$product','$qty')";
-  $executeinsert=mysqli_query($connect,$insert);
-  if ($executeinsert) {
-   echo "product created well";
-
-  }
-  else{
-   echo "failed to create product";
-  }
-
-}
+         $insert = "INSERT INTO products values ('','$user_id','$product','$qty')";
+         $executeinsert = mysqli_query($connect, $insert);
+         if ($executeinsert) {
+            echo "product created well";
+         } else {
+            echo "failed to create product";
+         }
+      }
 
       ?>
       <br>
       <div>
-       <h3>
-         my product
-       </h3>
+         <h3>
+            my product
+         </h3>
 
-       <?php
-        $checkproduct="SELECT * FROM products where user_id='$user_id'";
-        $executeProductQuery=mysqli_query($connect,$checkproduct);
-        $record=mysqli_num_rows($executeProductQuery);
-        if ($record) {
-         echo "
+         <?php
+         $checkproduct = "SELECT * FROM products where user_id='$user_id'";
+         $executeProductQuery = mysqli_query($connect, $checkproduct);
+         $record = mysqli_num_rows($executeProductQuery);
+         if ($record) {
+            echo "
          <table>
 
          <tr>
@@ -89,27 +86,25 @@ if (isset($_POST['upload'])) {
          <td>product quantity</td>
          </tr>
          ";
-     $i=1;
-         while($row=mysqli_fetch_array($executeProductQuery))
-         {
-            $prod=$row['pro_name'];
-            $qty=$row['pro_qty'];
-            echo "<tr>
+            $i = 1;
+            while ($row = mysqli_fetch_array($executeProductQuery)) {
+               $prod = $row['pro_name'];
+               $qty = $row['pro_qty'];
+               echo "<tr>
             <th>$i</th>
             <th>$prod</th>
             <th>$qty</th>
 
             </tr>";
 
-            $i++;
+               $i++;
+            }
+         } else {
+
+            echo "no product updated yet";
          }
-        }
-        else{
 
-         echo "no product updated yet";
-        }
-
-       ?>
+         ?>
       </div>
    </center>
 </body>
